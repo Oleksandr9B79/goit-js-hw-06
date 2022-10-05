2ce3bc5706b7c9b3d7a115021a23ae06502c69ad
Oleksandr Bazavliak <106233191+Oleksandr9B79@users.noreply.github.com>
2022-10-05 10:27:24 +0300

    finish1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # goit-js-hw-06
+
+<!-- task-2
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+const itemEl = [];
+
+ingredients.map((ingredient) => {
+  const element = document.createElement("li");
+  element.textContent = ingredient;
+  element.classList.add("item");
+
+  itemEl.push(element);
+  list.append(element);
+});
