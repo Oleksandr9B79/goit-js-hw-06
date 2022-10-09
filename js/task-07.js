511a52cef9805cdc71384b5e95021b26984e00c3
Oleksandr Bazavliak <106233191+Oleksandr9B79@users.noreply.github.com>
2022-10-09 19:52:08 +0300

    finish

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const spanEl = document.querySelector("#text");
 const inputEl = document.querySelector("#font-size-control");
-
+spanEl.style.fontSize = `${inputEl.value}px`;
 inputEl.addEventListener("input", (event) => {
   spanEl.style.fontSize = `${event.currentTarget.value}px`;
   console.log((spanEl.style.fontStyle = `${event.currentTarget.value}px`));
